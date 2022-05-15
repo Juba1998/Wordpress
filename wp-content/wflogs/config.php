b7ca6ddcdb47382dfbe407e7637594229bfcc225
Juba1998 <juba.harf@gmail.com>
2022-05-15 15:26:54 +0200

    first commit

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1651653027;s:7:"authKey";s:64:"fB-LGsR7]Pg5AO!Y dgYgbe=[,Z4uA rq`dzlTNoekLLI;ABVopG/..[VV`zz$.|";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2564;}
